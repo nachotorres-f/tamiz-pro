ed115db3804e566fbf68b830f357dadd49f196b5
Ignacio Alejandro Torres Franceschi <itorresfranceschi@icloud.com>
2025-08-06 10:19:40 -0300

    Mejorar botones de siguiente/anterior semana

--- src/components/navegacionSemanal.tsx
+++ src/components/navegacionSemanal.tsx
@@ -1,4 +1,5 @@
 'use cliente';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 import { addDays } from 'date-fns';
 
@@ -9,17 +10,27 @@ export function NavegacionSemanal({
     semanaBase: Date;
     setSemanaBase: (value: Date) => void;
 }) {
+    const [contador, setContador] = React.useState(0);
     return (
-        <div className="d-flex justify-content-between mb-3">
-            <Button
-                size="sm"
-                onClick={() => setSemanaBase(addDays(semanaBase, -7))}>
-                ⬅ Semana anterior
-            </Button>
+        <div className="d-flex justify-content-end mb-3">
+            {contador > 0 && (
+                <Button
+                    size="sm"
+                    className="me-2"
+                    onClick={() => {
+                        setSemanaBase(addDays(semanaBase, -7));
+                        setContador(contador - 1);
+                    }}>
+                    ⬅ Semana anterior
+                </Button>
+            )}
 
             <Button
                 size="sm"
-                onClick={() => setSemanaBase(addDays(semanaBase, 7))}>
+                onClick={() => {
+                    setSemanaBase(addDays(semanaBase, 7));
+                    setContador(contador + 1);
+                }}>
                 Semana siguiente ➡
             </Button>
         </div>
