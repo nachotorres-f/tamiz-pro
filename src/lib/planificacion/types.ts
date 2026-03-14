07d2fcc6ba258fbe9041735e6778ffae7c3e0d36
Ignacio Alejandro Torres Franceschi <itorresfranceschi@icloud.com>
2026-03-14 20:50:43 -0300

    Refactoriza página de planificación en componentes reutilizables

---
+++ src/lib/planificacion/types.ts
@@ -0,0 +1,8 @@
+export interface EventoPlanificacion {
+    id: number;
+    fecha: string;
+    lugar: string;
+    nombre: string;
+    salon: string;
+    deshabilitadaPlanificacion?: boolean;
+}
